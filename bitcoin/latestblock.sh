4869f441457e3d847a754d2b1cf79e55abb63603
Caleb Clark <calebjclark@gmail.com>
2025-04-22 22:28:35 -0500

    feat: added bidder bot to docker-compose.yaml + some other improvements

--- bitcoin/latestblock.sh
+++ bitcoin/latestblock.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 localhost_block_number=$(bitcoin-cli --conf="$BITCOIN_CONFIG" getblockchaininfo | jq -r '.blocks')
-localhost_synced=true #$(bitcoin-cli --conf="$BITCOIN_CONFIG" getindexinfo | jq -r '.synced')
 
 if [ "$BITCOIN_CHAIN" = "signet" ]; then
   mainchain_block_number=$(bitcoin-cli --conf="$BITCOIN_CONFIG" getpeerinfo | jq 'map(.startingheight) | max')
@@ -16,11 +15,4 @@ if [[ -z "$mainchain_block_number" || -z "$localhost_block_number" ]]; then
   exit 1
 fi
 
-# Calculate sync percentage (capped at 100%)
-sync_pct=$(awk -v local="$localhost_block_number" -v main="$mainchain_block_number" 'BEGIN { pct = (local / main) * 100; print (pct > 99 ? 99 : (pct < 0 ? 0 : pct)) }' | xargs printf "%.2f")
-
-if [ "$localhost_synced" = "true" ]; then
-  sync_pct=$(awk -v n="$sync_pct" 'BEGIN { printf "%.2f", n + 1 }')
-fi
-
-echo "$sync_pct%"
+echo "$localhost_block_number-$mainchain_block_number"
